766a761ccec09c61c3201f7854b2b0f2148d0acf
Paweł Liczmański <paweliczek@gmail.com>
2018-08-16 21:00:02 +0200

    beginning of project

---
+++ IntermediateTrainingCoreData/IntermediateTrainingCoreData/CompaniesController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IntermediateTrainingCoreData
+//
+//  Created by Paweł Liczmański on 16.08.2018.
+//  Copyright © 2018 Paweł Liczmański. All rights reserved.
+//
+
+import UIKit
+
+class CompaniesController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		tableView.backgroundColor = .yellow
+		
+
+	}
+
+	
+
+}
+
