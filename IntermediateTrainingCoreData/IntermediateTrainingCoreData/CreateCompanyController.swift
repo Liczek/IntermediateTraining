869aef0505610690de8c35feab122a57de264061
Paweł Liczmański <paweliczek@gmail.com>
2018-08-17 19:18:42 +0200

    AutoLayaout CreateCompanyController

--- IntermediateTrainingCoreData/IntermediateTrainingCoreData/CreateCompanyController.swift
+++ IntermediateTrainingCoreData/IntermediateTrainingCoreData/CreateCompanyController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class CreateCompanyController: UIViewController {
 	
+	let lightBlueBackgroudView: UIView = {
+		let view = UIView()
+		view.backgroundColor = .lightBlue
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	let nameLabel: UILabel = {
+		let label = UILabel()
+		label.text = "Name"
+		label.font = UIFont.boldSystemFont(ofSize: 16)
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	let nameTextField: UITextField = {
+		let textField = UITextField()
+		textField.placeholder = "Enter name of company"
+		textField.font = UIFont.boldSystemFont(ofSize: 16)
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		return textField
+	}()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -17,9 +40,66 @@ class CreateCompanyController: UIViewController {
 		title = "Create Company"
 		let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonHandle))
 		navigationItem.setLeftBarButton(cancelButton, animated: true)
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave))
+		
+		view.addSubview(lightBlueBackgroudView)
+		view.addSubview(nameLabel)
+		view.addSubview(nameTextField)
+		setConstraints()
 	}
 	
 	@objc func cancelButtonHandle() {
 		dismiss(animated: true, completion: nil)
 	}
+	
+	@objc func handleSave() {
+		print("Save new company")
+	}
+	
+	func setConstraints() {
+		
+		let lightBlueBGViewConstraints = [
+			lightBlueBackgroudView.leftAnchor.constraint(equalTo: view.leftAnchor),
+			lightBlueBackgroudView.rightAnchor.constraint(equalTo: view.rightAnchor),
+			lightBlueBackgroudView.topAnchor.constraint(equalTo: view.topAnchor),
+			lightBlueBackgroudView.heightAnchor.constraint(equalToConstant: 50)
+		]
+		lightBlueBGViewConstraints.forEach { (constraint) in
+			constraint.isActive = true
+		}
+		
+		
+		let nameLabelConstraints = [
+		nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+		nameLabel.widthAnchor.constraint(equalToConstant: 100),
+		nameLabel.topAnchor.constraint(equalTo: view.topAnchor),
+		nameLabel.heightAnchor.constraint(equalToConstant: 50)
+		]
+		nameLabelConstraints.forEach { (constraint) in
+			constraint.isActive = true
+		}
+
+		let nameTextFieldConstraints = [
+			nameTextField.leftAnchor.constraint(equalTo: nameLabel.rightAnchor),
+			nameTextField.topAnchor.constraint(equalTo: nameLabel.topAnchor),
+			nameTextField.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor),
+			nameTextField.rightAnchor.constraint(equalTo: view.rightAnchor)
+		]
+		nameTextFieldConstraints.forEach { (constraint) in
+			constraint.isActive = true
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
