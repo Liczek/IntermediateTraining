67f65f01a303064d001e059ecc3e6999952ff2e2
Paweł Liczmański <paweliczek@gmail.com>
2018-08-17 23:07:27 +0200

    test nauka 1

--- IntermediateTrainingCoreData/IntermediateTrainingCoreData/CompaniesController.swift
+++ IntermediateTrainingCoreData/IntermediateTrainingCoreData/CompaniesController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 class CompaniesController: UITableViewController, CreateCompanyControllerDelegate {
-	//test
+	//test nauka 1
 	var companies = [Company]()
 	
 	override func viewDidLoad() {
